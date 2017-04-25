af96d4b8f7d315d904d94467ee32459afc7674c5
PA373534 <PA373534@L-156101991.wipro.com>
2017-04-25 10:07:51 -0500

    ya quedo

--- src/test/java/com/city/paypoints/controller/PayWithPointsControllerTest.java
+++ src/test/java/com/city/paypoints/controller/PayWithPointsControllerTest.java
@@ -1,21 +1,4 @@
-<<<<<<< HEAD:src/test/java/com/city/pay/with/points/controller/PayWithPointsControllerTest.java
-package com.city.pay.with.points.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.citibanamex.api.pay.with.points.controller.PayPointsController;
-
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = PayPointsController.class)
-@ComponentScan("com.citibanamex.api.paypoints")
-public class PayWithPointsControllerTest {
-
-}
-=======
 package com.city.paypoints.controller;
 
 import org.junit.runner.RunWith;
@@ -32,4 +15,4 @@ import com.citibanamex.api.paypoints.controller.PayPointsController;
 public class PayWithPointsControllerTest {
 
 }
->>>>>>> 1d9eb14e4ad95d96de24cd211953dd64dab171ee:src/test/java/com/city/paypoints/controller/PayWithPointsControllerTest.java
+
