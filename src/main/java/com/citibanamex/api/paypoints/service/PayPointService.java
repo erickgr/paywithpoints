06ad873f7444c60ae9c4bcbb5976a8f8ce8aa980
PA373534 <PA373534@L-156101991.wipro.com>
2017-04-25 11:26:12 -0500

    11:26

--- src/main/java/com/citibanamex/api/paypoints/service/PayPointService.java
+++ src/main/java/com/citibanamex/api/paypoints/service/PayPointService.java
@@ -3,17 +3,18 @@ package com.citibanamex.api.paypoints.service;
 import com.citibanamex.api.paypoints.model.IsEligible;
 import com.citibanamex.api.paypoints.model.LinkStatus;
 import com.citibanamex.api.paypoints.model.PointBalance;
+import com.citibanamex.api.paypoints.model.RedemptionOrderSummary;
 
 public interface PayPointService {
 	
-	public IsEligible checkRewards(String cloakedCard);
+	IsEligible checkRewards(String cloakedCard);
 	
-	public void  updatePayPoints();
+	void  updatePayPoints();
 	
-	public LinkStatus createLinkCode();
+	LinkStatus createLinkCode();
 	
-	public PointBalance retrieveRewards();
+	PointBalance retrieveRewards();
 	
-	public void redeemReward();
+	RedemptionOrderSummary redeemReward();
 	
 }
