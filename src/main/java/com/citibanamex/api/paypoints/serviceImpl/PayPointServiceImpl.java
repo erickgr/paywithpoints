c6799982da312a5a2078bc1b2997bb2d98ca4f97
Pablo Gonzalez Carrasco (MEXICO) <PA373534@wipro.com>
2017-04-25 11:35:47 -0500

    11:35

---
+++ src/main/java/com/citibanamex/api/paypoints/serviceImpl/PayPointServiceImpl.java
@@ -0,0 +1,29 @@
+package com.citibanamex.api.paypoints.serviceImpl;
+
+import com.citibanamex.api.paypoints.model.IsEligible;
+import com.citibanamex.api.paypoints.model.LinkStatus;
+import com.citibanamex.api.paypoints.model.PointBalance;
+import com.citibanamex.api.paypoints.model.RedemptionOrderSummary;
+
+public class PayPointServiceImpl {
+
+	public IsEligible checkRewards(String cloakedCard) {
+		return null;
+	}
+
+	public void updatePayPoints() {
+
+	}
+
+	public LinkStatus createLinkCode() {
+		return null;
+	}
+
+	public PointBalance retrieveRewards() {
+		return null;
+	}
+
+	public RedemptionOrderSummary redeemReward() {
+		return null;
+	}
+}
